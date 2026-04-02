f0dc3de8e96d09f755dd274c2f3426e028226cf9
pranshu-2853 <pranshu2853@gmail.com>
2026-04-03 01:12:47 +0530

    Initial project setup

---
+++ src/main/java/com/ticketing/booking/TicketBookingApplication.java
@@ -0,0 +1,14 @@
+package com.ticketing.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingApplication {
+
+	public static void main(String[] args) {
+        java.util.TimeZone.setDefault(java.util.TimeZone.getTimeZone("Asia/Kolkata"));
+		SpringApplication.run(TicketBookingApplication.class, args);
+	}
+
+}
